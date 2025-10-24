32385961bcd78ecec4219e4323eacbca94e99f20
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-10-24 12:25:59 -0400

    Ejercicio Finalizado

--- Actividad4LengProg3/Actividad4LengProg3/Program.cs
+++ Actividad4LengProg3/Actividad4LengProg3/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Estudiantes}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
